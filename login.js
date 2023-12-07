ee5ac0fcd9cb9edf96c536ac8cd9ac1ac9dbbd2d
FalconTheBerd <136229185+FalconTheBerd@users.noreply.github.com>
2023-12-08 08:18:35 +1000

    Update login.js

--- login.js
+++ login.js
@@ -13,10 +13,10 @@ document.addEventListener("DOMContentLoaded", function () {
   const loginButton = document.getElementById("loginButton");
 
   const users = [
-    { username: "LachlanDwyer", password: "beac5ecc982f82ee4d00161c68a989becb1aa8ca90980c87e07db167113c2dfd"}, // SHA-256 hash of "Falcon"
-    { username: "TerryTsoukalas", password: "8f9be23677a0c32b6c2a946ea50d1673525d4740870b245a064f59c6b24c6422" }, // SHA-256 hash of "Fortniteisbad"
-    { username: "Admin", password: "fd3f8cb72c0de9c620d239358caacc7b8b5e7420b80e54eab4b90c85de8f1d02" }, // SHA-256 hash of "Compass"
-    { username: "LalitaGracePrestonHaira", password: "5333a19ac109063be6ab54538de3191d6ed18e094c4f4409f65409feef171ca7" }, // SHA-256 hash of "Hell0!"
+    { username: "LachlanDwyer", password: "beac5ecc982f82ee4d00161c68a989becb1aa8ca90980c87e07db167113c2dfd"},
+    { username: "TerryTsoukalas", password: "8f9be23677a0c32b6c2a946ea50d1673525d4740870b245a064f59c6b24c6422" },
+    { username: "Admin", password: "fd3f8cb72c0de9c620d239358caacc7b8b5e7420b80e54eab4b90c85de8f1d02" },
+    { username: "LalitaGracePrestonHaira", password: "5333a19ac109063be6ab54538de3191d6ed18e094c4f4409f65409feef171ca7" },
   ];
 
   loginForm.addEventListener("submit", function (event) {
