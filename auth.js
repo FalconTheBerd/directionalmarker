062e34c717ca629ed9164e3bcfd8a1f63f6aae2d
FalconTheBerd <lachiedwyer123@gmail.com>
2024-04-13 11:30:53 +1000

    Added option to delete user on Admin Panel

--- auth.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const authenticated = localStorage.getItem("authenticated");
-    if (!authenticated || authenticated !== "true") {
-      window.location.href = "index.html";
-    }
-  });
-  
